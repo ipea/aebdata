4c4ac2c857a21859ee3a5b8f7a94993ff9643603
Hugo Macedo <22848633+hhmacedo@users.noreply.github.com>
2024-07-02 16:14:07 -0300

    Create the list_series function

--- R/list.R
+++ R/list.R
@@ -28,3 +28,104 @@ list_themes <- function() {
 
 }
 
+#' List created series
+#'
+#' List all series from the Atlas, regardless of whether they have data
+#' available for download or not.
+#'
+#' @param theme_id,theme_title Optional parameters that can be used individually
+#'  or combined to filter the selected themes.
+#'
+#' @return A data.frame
+#' @export
+#'
+#' @examples
+#' list_series()
+#' list_series(theme_id = c(42, 50))
+#' list_series(theme_id = 35, theme_title = c("Organizações do Estado"))
+
+list_series <- function(theme_id = NULL, theme_title = NULL) {
+
+  # List available themes
+  df_themes <- aebdata::list_themes()
+
+  # Check the need to filter specific themes
+  if (!is.null(theme_id) | !is.null(theme_title)) {
+
+    # Check if at least one theme_id or theme_title exists
+    if (length(c(intersect(theme_id, df_themes$theme_id),
+                 intersect(theme_title, df_themes$theme_title))) == 0) {
+      if (is.null(theme_id) & !is.null(theme_title)) {
+        stop("All theme_title's are missing")
+      } else if (!is.null(theme_id) & is.null(theme_title)) {
+        stop("All theme_id's are missing")
+      } else if (!is.null(theme_id) & !is.null(theme_title)) {
+        stop("All theme_id's and theme_title's are missing")
+      }
+    }
+
+    # Check for missing values and warn about them
+    if (length(setdiff(theme_id, df_themes$theme_id) > 0)) {
+      paste(
+        "Missing theme_id values:",
+        paste(setdiff(theme_id, df_themes$theme_id), collapse = ", ")
+      ) |>
+        warning()
+    }
+    if (length(setdiff(theme_title, df_themes$theme_title) > 0)) {
+      paste(
+        "Missing theme_title values:",
+        paste(setdiff(theme_title, df_themes$theme_title), collapse = ", ")
+      ) |>
+        warning()
+    }
+
+    # Filter the selected themes
+    df_themes <- df_themes[df_themes$theme_id %in% theme_id |
+                             df_themes$theme_title %in% theme_title, ]
+
+  }
+
+  # List series from each theme
+  ls_series <- df_themes$theme_id |>
+    lapply(function(id) list_series_id(theme_id = id))
+
+  vc_theme_rep <- sapply(ls_series, nrow)
+
+  # Bind the series
+  df_series <- cbind(
+    theme_title = rep(df_themes$theme_title, times = vc_theme_rep),
+    theme_id = rep(df_themes$theme_id, times = vc_theme_rep),
+    do.call(rbind, ls_series)
+  )
+
+  rm(vc_theme_rep, df_themes)
+
+  return(df_series)
+
+}
+
+#' List series from a specific theme
+#'
+#' @param theme_id The theme's id to return series from.
+#'
+#' @return A data.frame containing serie_title and serie_id.
+#'
+#' @examples
+#' list_series_id(theme_id = 26)
+#'
+#' @noRd
+
+list_series_id <- function(theme_id) {
+  df_series <- "https://www.ipea.gov.br/atlasestado/api/v1/series/" |>
+    paste0(theme_id) |>
+    httr2::request() |>
+    httr2::req_perform() |>
+    httr2::resp_body_json() |>
+    lapply(`[`, c("titulo", "id")) |>
+    do.call(rbind.data.frame, args = _)
+
+  names(df_series) <- c("series_title", "series_id")
+
+  return(df_series)
+}
