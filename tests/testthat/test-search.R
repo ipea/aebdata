337cdfe05715cd8f36b77d19004569486a3c1a1f
Hugo Macedo <22848633+hhmacedo@users.noreply.github.com>
2024-07-04 15:31:00 -0300

    Skip the search test if stringi is not installed

--- tests/testthat/test-search.R
+++ tests/testthat/test-search.R
@@ -1,8 +1,10 @@
 test_that("multiple search works", {
+  skip_if_not_installed("stringi")
   expect_snapshot(search_series(c("militares", "cargos"), require_all = TRUE))
 })
 
 test_that("return empty", {
+  skip_if_not_installed("stringi")
   expect_equal(search_series("miiltares"),
                data.frame(series_title = character(), series_id = integer(),
                           theme_title = character(), theme_id = integer()))
