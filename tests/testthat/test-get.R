ce3025dc5c8c2ccabad97fd36e81272151324022
Hugo Macedo <22848633+hhmacedo@users.noreply.github.com>
2024-07-12 16:32:33 -0300

    Update the get test

--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -1,4 +1,36 @@
+test_that("missing parameters", {
+  expect_error(get_series())
+})
+
+test_that("wrong parameters", {
+  # Totally wrong
+  expect_error(get_series(series_id = 1))
+  expect_error(get_series(series_title = "Test"))
+  expect_error(get_series(series_id = 1, series_title = "Test"))
+  # Partially wrong
+  expect_warning(get_series(series_id = c(1, 215)))
+  expect_warning(get_series(series_id = 215, series_title = "Test"))
+})
+
+test_that("empty series", {
+  # all empty
+  expect_error(get_series(series_id = 162))
+  expect_error(get_series(series_id = c(162, 177)))
+  # one works
+  expect_warning(get_series(series_id = c(162, 215)))
+})
+
 test_that("get works", {
-  expect_snapshot(get_series(series_id = 215))
-  expect_snapshot(get_series(series_id = c(215, 219)))
+  rds <- readRDS(test_path("_data", "get.rds"))
+  expect_equal(get_series(series_id = 215), rds$one)
+  expect_equal(get_series(series_id = c(215, 197)),
+               list(`215` = rds$one,
+                    `197` = rds$multiple))
+})
+
+test_that("get_csv works", {
+  rds <- readRDS(test_path("_data", "get.rds"))
+  expect_null(get_series_csv(162))
+  expect_equal(get_series_csv(215), rds$one)
+  expect_equal(get_series_csv(197), rds$multiple)
 })
